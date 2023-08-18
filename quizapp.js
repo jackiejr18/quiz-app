b48da89d4423370f145022b5e911b7c7b1fe5d58
Jackie Jr <jwysinger@gmail.com>
2023-08-17 21:48:44 -0500

    added function for answer button

--- quizapp.js
+++ quizapp.js
@@ -58,5 +58,14 @@ function showQuestion() {
 	let currentQuestion = questions[currentQuestionIndex];
 	let questionNo = currentQuestionIndex + 1;
 	questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+	currentQuestion.answers.forEach(answer => {
+		const button = document.createElement("button");
+		button.innerHTML = answer.text;
+		button.classList.add("btn");
+		answerButton.appendChild(button);
+	});
 }
+
+
 
