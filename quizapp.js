bd51fa7f53577bb92c2a8f62ae56385ab357ee40
Jackie Jr <jwysinger@gmail.com>
2023-08-17 21:30:51 -0500

    added variables to javascript file

--- quizapp.js
+++ quizapp.js
@@ -39,3 +39,11 @@ const questions = [
 		]
 	}
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
