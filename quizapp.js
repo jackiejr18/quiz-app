45be0e5e03ac5199cf0eb37f4503c4d9aa32657c
Jackie Jr <jwysinger@gmail.com>
2023-08-17 21:40:12 -0500

    added functions to start quiz and display questions

--- quizapp.js
+++ quizapp.js
@@ -47,3 +47,16 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
+function startQuiz() {
+	currentQuestionIndex = 0;
+	score = 0;
+	nextButton.innerHTML = "Next";
+	showQuestion(); 
+}
+
+function showQuestion() {
+	let currentQuestion = questions[currentQuestionIndex];
+	let questionNo = currentQuestionIndex + 1;
+	questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+}
+
